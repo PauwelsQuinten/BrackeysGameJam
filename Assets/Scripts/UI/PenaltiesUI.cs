6bfd504897c632e4ae7e6fb86ba82bb1cf1daab0
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-08-31 09:16:02 +0200

    implemented new art

--- Assets/Scripts/UI/PenaltiesUI.cs
+++ Assets/Scripts/UI/PenaltiesUI.cs
@@ -7,25 +7,25 @@ public class PenaltiesUI : MonoBehaviour
     [SerializeField]
     private List<Image> _penaltyImages = new List<Image>();
 
+    [SerializeField]
+    private Sprite _brokenCookie;
+    [SerializeField]
+    private Sprite _fullCookie;
+
 
     public void PenaltiesChanged(Component sender, object obj)
     {
         int penalty = (int)(obj as int?);
         if (penalty > 3) return;
-        Color newColor = Color.white;
         if (penalty == 0)
         {
             foreach(Image image in _penaltyImages)
             {
-                newColor = image.color;
-                newColor.a = 1;
-                image.color = newColor;
+                image.sprite = _fullCookie;
             }
             return;
         }
 
-        newColor = _penaltyImages[penalty - 1].color;
-        newColor.a = 0.5f;
-        _penaltyImages[penalty - 1].color = newColor;
+        _penaltyImages[penalty - 1].sprite = _brokenCookie;
     }
 }
